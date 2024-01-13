cbf973a6f25e7f07aa2c86ce40c87424413c698e
najnaj20 <portofnajwa@gmail.com>
2024-01-13 10:07:23 +0800

    tp2

---
+++ windows/tp2/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::tp2::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::tp2::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
